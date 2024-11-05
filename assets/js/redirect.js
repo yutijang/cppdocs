6ec01f055c3e5aac8757e774d4bea854fcf897f9
yutijang <yutijang@gmail.com>
2024-11-05 23:29:57 +0700

    Update redirect.js

--- assets/js/redirect.js
+++ assets/js/redirect.js
@@ -1,17 +1,1 @@
-  document.addEventListener("DOMContentLoaded", function() {
-    const anchor = window.location.hash;
-    const anchorMap = {
-      "#containers": "/cppdocs/docs/containers/#containers",
-      "#array": "/cppdocs/docs/array/#array",
-      "#vector": "/cppdocs/docs/vector/#vector"
-    };
-
-    if (anchor) {
-      const targetUrl = anchorMap[anchor];
-      if (targetUrl) {
-        window.location.href = targetUrl;
-      } else {
-        window.location.href = "/cppdocs/404/";
-      }
-    }
-  });
+document.addEventListener("DOMContentLoaded",function(){let o=window.location.hash;if(o){let c={"#containers":"/cppdocs/docs/containers/#containers","#array":"/cppdocs/docs/array/#array","#vector":"/cppdocs/docs/vector/#vector"}[o];c?window.location.href=c:window.location.href="/cppdocs/404/"}});
