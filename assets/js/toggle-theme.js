f6d105ec2e4bd9bd10acfcb21bc7d8c0ed179919
yutijang <yutijang@gmail.com>
2024-11-05 20:19:21 +0700

    Create toggle-theme.js

---
+++ assets/js/toggle-theme.js
@@ -0,0 +1,15 @@
+// File: assets/js/toggle-theme.js
+
+document.addEventListener("DOMContentLoaded", function() {
+    const toggleDarkMode = document.querySelector('.js-toggle-dark-mode');
+
+    jtd.addEvent(toggleDarkMode, 'click', function() {
+        if (jtd.getTheme() === 'dark') {
+            jtd.setTheme('light');
+            toggleDarkMode.textContent = 'Preview dark color scheme';
+        } else {
+            jtd.setTheme('dark');
+            toggleDarkMode.textContent = 'Return to the light side';
+        }
+    });
+});
